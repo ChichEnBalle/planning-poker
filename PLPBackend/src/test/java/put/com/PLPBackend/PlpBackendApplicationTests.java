7a2f63d969f18a5a44af1fac7d0a6cbff5e12598
fquinaou <francois.quinaou@etudiant.univ-rennes1.fr>
2025-03-25 11:08:43 +0100

    init backend + frontend + docker

---
+++ PLPBackend/src/test/java/put/com/PLPBackend/PlpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package put.com.PLPBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ plpfrontend/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	let message = "Chargement...";
+  
+	async function fetchMessage() {
+		const res = await fetch("http://backend:8080/api/hello");
+		message = await res.text();
+	}
+  
+	fetchMessage();
+  </script>
+  
+  <h1>{message}</h1>
+  
