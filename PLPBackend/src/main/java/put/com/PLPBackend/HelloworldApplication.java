92984ae5d446e2748c510c43bc6751381348f6fd
Lucas Tommi <tommi.lucas@etudiant.univ-rennes1.fr>
2025-04-14 22:33:08 +0200

    correction de bugs docker

--- PLPBackend/src/main/java/put/com/PLPBackend/HelloworldApplication.java
+++ PLPBackend/src/main/java/put/com/PLPBackend/HelloworldApplication.java
@@ -5,8 +5,12 @@ package put.com.PLPBackend;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+// @PropertySource("classpath:application.properties")
+@EnableConfigurationProperties
 public class HelloworldApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
