0139cecbc88b2510a2bb126f36b10da480aa70b0
fquinaou <francois.quinaou@etudiant.univ-rennes1.fr>
2025-04-14 19:37:46 +0200

    docker compose fonctionnel

--- plpfrontend/src/routes/+page.svelte
+++ plpfrontend/src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
+	import { onMount } from 'svelte';
+  
 	let message = "Chargement...";
   
-	async function fetchMessage() {
-		const res = await fetch("http://localhost:8080/api/hello");
-		message = await res.text();
-	}
-  
-	fetchMessage();
+	onMount(async () => {
+	  const res = await fetch("http://localhost:8080/api/hello");
+	  message = await res.text();
+	});
   </script>
   
   <h1>{message}</h1>
